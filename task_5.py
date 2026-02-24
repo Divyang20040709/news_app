213381b3890014b4b34cdbe3f5283bb59f9f1b8a
Divyang Solanki <divyangsolanki2004@gmail.com>
2026-02-24 15:17:14 +0530

    Add files via upload
    I have made a News App using streamlit, Json, Requests and API

---
+++ task_5.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import json
+import requests
+
+st.title("News App")
+response = requests.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=829a47d11a3a4d7c82a76890d9b942a3')
+data_news = response.json()
+articles = data_news['articles']
+for article in articles:
+    st.subheader(article['title'])
+    st.write(article['description'])
+    st.write(f"Published at: {article['publishedAt']}")
+    st.write(f"Source: {article['source']['name']}")
+
+with open('news_data.json', mode='w', encoding='utf-8') as newsfile:
+    json.dump(data_news, newsfile, indent=4)
