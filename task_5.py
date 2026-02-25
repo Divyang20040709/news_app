e314cdfc2153ef097e2b77c8f05866fad3427616
Divyang Solanki <divyangsolanki2004@gmail.com>
2026-02-25 14:58:08 +0530

    Enhance news app with images and multiple sources
    Added image display for articles and fetched news from multiple sources.

--- task_5.py
+++ task_5.py
@@ -11,4 +11,31 @@ for article in articles:
     st.write(article['description'])
     st.write(f"Published at: {article['publishedAt']}")
     st.write(f"Source: {article['source']['name']}")
+    img_url = article.get('urlToImage')
+    if img_url:
+        st.image(img_url)
 
+response = requests.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=829a47d11a3a4d7c82a76890d9b942a3')
+data_news = response.json()
+articles = data_news['articles']
+for article in articles:
+    st.subheader(article['title'])
+    st.write(article['description'])
+    st.write(f"Published at: {article['publishedAt']}")
+    st.write(f"Source: {article['source']['name']}")
+    img_url = article.get('urlToImage')
+    if img_url:
+        st.image(img_url)
+
+
+response = requests.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=829a47d11a3a4d7c82a76890d9b942a3')
+data_news = response.json()
+articles = data_news['articles']
+for article in articles:
+    st.subheader(article['title'])
+    st.write(article['description'])
+    st.write(f"Published at: {article['publishedAt']}")
+    st.write(f"Source: {article['source']['name']}")
+    img_url = article.get('urlToImage')
+    if img_url:
+        st.image(img_url)
